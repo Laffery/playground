1993dc61dc6b5b4eac535bc715da55ced2fd4d74
laffery <2387065420@qq.com>
2022-04-03 12:28:48 +0800

    feat(ts): 898 easy includes

---
+++ ts-challenge/898-easy-includes.ts
@@ -0,0 +1,47 @@
+import { Equal, Expect, NotAny } from "utils";
+
+/**
+ * Implement the JavaScript Array.includes function in the type system. A type takes the two arguments.
+ * The output should be a boolean true or false.
+ * see https://github.com/type-challenges/type-challenges/blob/master/questions/898-easy-includes/README.md
+ */
+
+/** Your code */
+type Includes<T extends readonly any[], P> = true extends {
+  [K in keyof T]: Equal<T[K], P>;
+}[number]
+  ? true
+  : false;
+
+type a = Includes<[false], false>;
+
+/**
+ * @note You can write your note here
+ * if there is some element type in the array that is equal to P, 
+ * then the result type if `true`(all elements are equal to P) or `boolean`(part of elements),
+ * anyway, the `true` extends to both of them.
+ */
+
+/** Test cases */
+type cases = [
+  Expect<
+    Equal<Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Kars">, true>
+  >,
+  Expect<
+    Equal<Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Dio">, false>
+  >,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 7>, true>>,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 4>, false>>,
+  Expect<Equal<Includes<[1, 2, 3], 2>, true>>,
+  Expect<Equal<Includes<[1, 2, 3], 1>, true>>,
+  Expect<Equal<Includes<[{}], { a: "A" }>, false>>,
+  Expect<Equal<Includes<[boolean, 2, 3, 5, 6, 7], false>, false>>,
+  Expect<Equal<Includes<[true, 2, 3, 5, 6, 7], boolean>, false>>,
+  Expect<Equal<Includes<[false, 2, 3, 5, 6, 7], false>, true>>,
+  Expect<Equal<Includes<[{ a: "A" }], { readonly a: "A" }>, false>>,
+  Expect<Equal<Includes<[{ readonly a: "A" }], { a: "A" }>, false>>,
+  Expect<Equal<Includes<[1], 1 | 2>, false>>,
+  Expect<Equal<Includes<[1 | 2], 1>, false>>,
+  Expect<Equal<Includes<[null], undefined>, false>>,
+  Expect<Equal<Includes<[undefined], null>, false>>
+];

069c05c57e5f969abd8a573324509ca07198ba57
laffery <2387065420@qq.com>
2022-04-03 10:00:51 +0800

    feat(ts): 189 esay awaited

---
+++ ts-challenge/189-easy-awaited.ts
@@ -0,0 +1,35 @@
+import { Equal, Expect, NotAny } from "utils";
+
+/**
+ * If we have a type which is wrapped type like Promise. How we can get a type which is inside the wrapped type?
+ * For example if we have Promise<ExampleType> how to get ExampleType?
+ * see https://github.com/type-challenges/type-challenges/blob/master/questions/189-easy-awaited/README.md
+ */
+
+/** Your code */
+type MyAwaited<T extends Promise<any>> = T extends Promise<infer U>
+  ? U extends Promise<any>
+    ? Awaited<U>
+    : U
+  : T;
+
+/**
+ * @note You can write your note here
+ * recursively unwrap the type until it is not a Promise type
+ */
+
+/** Test cases */
+type X = Promise<string>;
+type Y = Promise<{ field: number }>;
+type Z = Promise<Promise<string | number>>;
+type A = Promise<Promise<Promise<number>>>;
+
+type cases = [
+  Expect<Equal<MyAwaited<X>, string>>,
+  Expect<Equal<MyAwaited<Y>, { field: number }>>,
+  Expect<Equal<MyAwaited<Z>, string | number>>,
+  Expect<Equal<MyAwaited<A>, number>>
+];
+
+// @ts-expect-error
+type error = MyAwaited<number>;
